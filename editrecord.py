d4ce04c66b02891ae1f01849187c05a257616139
Becky Yoose <b.yoose@gmail.com>
2013-04-02 12:55:59 -0500

    Lots of changes, should be functional
    It's running for me, at least...
    I've changed several variables, edited the comments and readme file, and
    added an example set of MARC records.

--- editrecord.py
+++ editrecord.py
@@ -1,28 +1,25 @@
-#yeah, this also needs cleaning up -> grokked from the main script
 import re
 import sys
 import string
-import shutil
-import pymarc
 import os
 from pymarc import Record, Field, MARCReader, MARCWriter
 
 
-#take out the '' in the date -> command line had the date in there. Need to insert an actual date here
-date = re.sub("'", "", date)
-
 #from the main script, the for loop started when the script was working with the spreadsheet of identifiers. 
-#I've modified the loop but haven't tested to see if this works yet or not.
+#I've modified the loop and have tested it, but there still might be bugs to squish.
 #SHIP SHIP SHIP! Fixes coming...
 
-reader = pymarc.MARCReader(open('marc.mrc'))
+#open the file of marc records, exampledump.mrc, which should be in the same place as this script.
+#the unicode/utf8 handling code is from https://github.com/edsu/pymarc/issues/7#issuecomment-1346308
+records = MARCReader(open('exampledump.mrc'),to_unicode=True, force_utf8=True, utf8_handling='ignore')
 
-for marc in reader:	
+#set date variable
+date = '2013-05-02'
 
-	record = Record(marc)
+for marc in records:	
 	
 	#get the bib record from the 907 field prior to deletion
-	n = record.get_fields('907')
+	n = marc.get_fields('907')
 	for field in n:
 		bib_rec_num_raw = field.get_subfields('a')
 
@@ -32,12 +29,12 @@ for marc in reader:
 	bib_rec_num = bib_rec_raw1.lstrip('[\'')
 
 	#delet 947 field
-	for f in record.get_fields('947'):
+	for f in marc.get_fields('947'):
 		  if f['a'] != '':
-			  record.remove_field(f) 
+			  marc.remove_field(f) 
 
 	#add 590 local note for display in the library catalog
-	record.add_field(
+	marc.add_field(
 	Field(
 		tag = '590',
 		indicators = [' ',' '],
@@ -46,7 +43,7 @@ for marc in reader:
 		]))	
 	
 	#add 790 local additional author field to indicate the donor. Our catalog does index the 79X fields in the author index.
-	record.add_field(
+	marc.add_field(
 	Field(
 		tag = '790',
 		indicators = ['1',' '],
@@ -56,7 +53,7 @@ for marc in reader:
 		]))
 		
 	#add 947 local field to indicate that this PDA ebook was purchased via the 'ebrar$supo' local code
-	record.add_field(
+	marc.add_field(
 		Field(
 			tag = '947',
 			indicators = [' ',' '],
@@ -65,7 +62,7 @@ for marc in reader:
 			]))			
 
 	#add 949 local field for overlay of bib record and creation of order record when record is uploaded into Millennium
-	record.add_field(
+	marc.add_field(
 		Field(
 			tag = '949',
 			indicators = [' ',' '],
@@ -74,16 +71,19 @@ for marc in reader:
 			]))	
 
 	#delete 907, 998, 910, 945 fields
-	for f in record.get_fields('907', '998', '910', '945'):
+	for f in marc.get_fields('907', '998', '910', '945'):
 		  if f['a'] != '':
-			  record.remove_field(f) 
-
+			  marc.remove_field(f) 
+	
+	#append record to a generic file.dat file
 	writer = MARCWriter(file('file.dat','a'))
-	writer.write(record)
-else :
-	cursor.close ()
-	conn.close ()
+	writer.write(marc)
 
-writer.close() #closes file.dat
-date = re.sub("-", "", date)
+#closes file.dat
+writer.close() 
+
+#take the date variable and get rid of the dashes
+date = re.sub("-", "", date) 
+
+#rename file.dat to the date in the date variable
 os.rename('file.dat', '%s.dat' %(date))
